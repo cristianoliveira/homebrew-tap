3a267fa36db13d9275c22e486bee7ba83d6af82e
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-05-08 17:14:22 -0300

    funzzy: version 0.3.0

--- funzzy.rb
+++ funzzy.rb
@@ -5,8 +5,8 @@
 class Funzzy < Formula
   desc "The Yet Another Fancy Watcher"
   homepage "https://github.com/cristianoliveira/funzzy"
-  url "https://github.com/cristianoliveira/funzzy/releases/download/v0.2.5/funzzy-v0.2.5-x86_64-apple-darwin.tar.gz"
-  version "0.2.5"
+  url "https://github.com/cristianoliveira/funzzy/releases/download/v0.3.0/funzzy-v0.3.0-x86_64-apple-darwin.tar.gz"
+  version "0.3.0"
   sha256 "b34ee2d8339a54f7edaf122394a5074ad9c77b554629560322f5fc70c29a6b39"
 
   def install
