66d49acfb08a7f059d8c25fa3f794f79d00a2d22
Cristian Oliveira <cristian.oliveiradarosa@barzahlen.de>
2017-08-21 10:45:16 +0200

    ergo: Bump version 0.1.0

--- ergo.rb
+++ ergo.rb
@@ -2,9 +2,9 @@ class Ergo < Formula
 
   desc 'Ergo Proxy - The local multiple services development made easy. No magic involved'
   homepage 'https://github.com/cristianoliveira/ergo'
-  url 'https://github.com/cristianoliveira/ergo/releases/download/v0.0.5/ergo-v0.0.5-darwin-amd64.tar.gz'
-  version 'v0.0.5'
-  sha256 'ac61f67628cf4890616fc36f5e7bfee03cf486885403fc2d1ca1c2546bb8add6'
+  url 'https://github.com/cristianoliveira/ergo/releases/download/v0.1.0/ergo-v0.1.0-darwin-amd64.tar.gz'
+  version 'v0.1.0'
+  sha256 'bca2fe69888558e26c3dbdc4b0241de4dc530fc6c43c3a634b4626e8dce74aed'
 
 
   def install
