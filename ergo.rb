5e79392da8d5f2684619d64349da87ccbeff99df
Cristian Oliveira <cristian.oliveiradarosa@barzahlen.de>
2017-09-10 16:43:38 +0200

    chore: version v0.1.5

--- ergo.rb
+++ ergo.rb
@@ -4,15 +4,7 @@ class Ergo < Formula
   homepage 'https://github.com/cristianoliveira/ergo'
   url 'https://github.com/cristianoliveira/ergo/releases/download/v0.1.5/ergo-v0.1.5-darwin-amd64.tar.gz'
   version 'v0.1.5'
-  sha256 'b46e32dbc38523073728ee53a2db06dfed36e30c935a7dabb3e15b93dd8f6bcc
-ergo
-v0
-1
-5
-darwin
-amd64
-tar
-gz'
+  sha256 'b46e32dbc38523073728ee53a2db06dfed36e30c935a7dabb3e15b93dd8f6bcc'
 
 
   def install
