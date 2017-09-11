e33d7c914782213b53412805a82f5446eb576078
Cristian Oliveira <cristian.oliveiradarosa@barzahlen.de>
2017-09-11 15:17:55 +0200

    chore: version v0.1.7

--- ergo.rb
+++ ergo.rb
@@ -2,9 +2,9 @@ class Ergo < Formula
 
   desc 'Ergo Proxy - The local multiple services development made easy. No magic involved'
   homepage 'https://github.com/cristianoliveira/ergo'
-  url 'https://github.com/cristianoliveira/ergo/releases/download/v0.1.6/ergo-v0.1.6-darwin-amd64.tar.gz'
-  version 'v0.1.6'
-  sha256 '3ed39b1ecfaff5f022687dfa3c2255f40e21707683bb85b35c98ba8f32ddfb73'
+  url 'https://github.com/cristianoliveira/ergo/releases/download/v0.1.7/ergo-v0.1.7-darwin-amd64.tar.gz'
+  version 'v0.1.7'
+  sha256 '0f0d03e79ae8c6eeb6420c761ef53852fbbe10e20aa3c070b2aadc61603bd2e8'
 
 
   def install
