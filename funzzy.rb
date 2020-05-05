0af6f592c7d79d14d838fc5cf3e38413c55b20cc
Dario Vladovic <d.vladimyr@gmail.com>
2020-05-05 03:41:03 +0200

    Update formulae
    - regenerating formulas with shellscripts

--- funzzy.rb
+++ funzzy.rb
@@ -1,14 +1,11 @@
 class Funzzy < Formula
-
-  desc 'The yet another fancy watcher'
-  homepage 'https://github.com/cristianoliveira/funzzy'
-  url 'https://github.com/cristianoliveira/funzzy/releases/download/v0.3.3/funzzy-v0.3.3-x86_64-apple-darwin.tar.gz'
-  version 'v0.3.3'
-  sha256 '32531a489ac50b4ec9321b4fbe6d246b61d18ee87f6b86638cfe5d2ed72ccb6d'
-
+  desc "The yet another fancy watcher"
+  homepage "https://github.com/cristianoliveira/funzzy"
+  url "https://github.com/cristianoliveira/funzzy/releases/download/v0.3.3/funzzy-v0.3.3-x86_64-apple-darwin.tar.gz"
+  version "0.3.3"
+  sha256 "32531a489ac50b4ec9321b4fbe6d246b61d18ee87f6b86638cfe5d2ed72ccb6d"
 
   def install
-    bin.install 'funzzy'
+    bin.install "funzzy"
   end
-
 end
