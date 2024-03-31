34ecc4cc70b02076afe77600b3e3b14b217e9de2
cristianoliveira <contato@cristianoliveira.com.br>
2024-03-31 12:35:19 +0200

    chore: version v0.3.2

--- ergo.rb
+++ ergo.rb
@@ -2,12 +2,13 @@ class Ergo < Formula
 
   desc 'Ergo Proxy - The local multiple services development made easy. No magic involved'
   homepage 'https://github.com/cristianoliveira/ergo'
-  url 'https://github.com/cristianoliveira/ergo/releases/download/v0.3.0/ergo-v0.3.0-darwin-amd64.tar.gz'
-  version 'v0.3.0'
-  sha256 '5c640d88b2bb3aa02b7d67b3277cdad2da51c212a8d2400ef575834d0116a1de'
+  url 'https://github.com/cristianoliveira/ergo/releases/download/v0.3.2/ergo-v0.3.2-darwin-amd64.tar.gz'
+  version 'v0.3.2'
+  sha256 'bba737fed59939aa74e4318b824601efc657eb10e854249d4e3ae459ed7c7337'
 
 
   def install
     bin.install 'ergo'
   end
+
 end
