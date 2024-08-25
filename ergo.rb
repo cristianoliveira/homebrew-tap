95fe9fd69a678ec713370f84853d1e18fbd1f79e
cristianoliveira <contato@cristianoliveira.com.br>
2024-08-25 11:11:55 +0200

    chore: version v0.4.1

--- ergo.rb
+++ ergo.rb
@@ -4,7 +4,7 @@ class Ergo < Formula
   homepage 'https://github.com/cristianoliveira/ergo'
   url 'https://github.com/cristianoliveira/ergo/releases/download/v0.4.1/ergo-v0.4.1-darwin-amd64.tar.gz'
   version 'v0.4.1'
-  sha256 ''
+  sha256 '13356de12d5038397c3deb2e4969c911f9eef125bf41dd520357605373fcda5a'
 
 
   def install
