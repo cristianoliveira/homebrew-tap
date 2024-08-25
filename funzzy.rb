eaa834eefd0e2cb4b9d033858732c2ad7f75c9d1
cristianoliveira <contato@cristianoliveira.com.br>
2024-08-25 13:49:46 +0200

    chore(funzzy): version v1.4.1

--- funzzy.rb
+++ funzzy.rb
@@ -2,9 +2,9 @@ class Funzzy < Formula
 
   desc 'The yet another fancy watcher'
   homepage 'https://github.com/cristianoliveira/funzzy'
-  url 'https://github.com/cristianoliveira/funzzy/releases/download/v1.3.0/funzzy-v1.3.0-x86_64-apple-darwin.tar.gz'
-  version 'v1.3.0'
-  sha256 '289441fa19c82bd4ce77f194052c4ec59334638b5e298fe7bbfbf6d49ebb23d0'
+  url 'https://github.com/cristianoliveira/funzzy/releases/download/v1.4.1/funzzy-v1.4.1-x86_64-apple-darwin.tar.gz'
+  version 'v1.4.1'
+  sha256 '8a75b62c20839bcfaff206392d84aaa33b7eb200dc3a134eff72c4479cd44159'
 
 
   def install
