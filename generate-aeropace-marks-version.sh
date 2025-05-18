d7375bbd95799b846c90d187ae06486d8877ec66
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-18 22:32:27 +0200

    update script

--- generate-aeropace-marks-version.sh
+++ generate-aeropace-marks-version.sh
@@ -2,24 +2,37 @@
 
 VERSION=`curl https://api.github.com/repos/cristianoliveira/aerospace-marks/releases/latest | jq .tag_name -r`
 
-BINARY_FILE_NAME="aerospace-marks-$VERSION-darwin-amd64.tar.gz"
-REPO_URL="https://github.com/cristianoliveira/aerospace-marks/releases/download/$VERSION/$BINARY_FILE_NAME"
+AMD_BINARY_FILE_NAME="aerospace-marks-$VERSION-darwin-amd64.tar.gz"
+AMD_REPO_URL="https://github.com/cristianoliveira/aerospace-marks/releases/download/$VERSION/$AMD_BINARY_FILE_NAME"
+ARM_BINARY_FILE_NAME="aerospace-marks-$VERSION-darwin-arm64.tar.gz"
+ARM_REPO_URL="https://github.com/cristianoliveira/aerospace-marks/releases/download/$VERSION/$ARM_BINARY_FILE_NAME"
 
-wget $REPO_URL
+wget $AMD_REPO_URL
+wget $ARM_REPO_URL
 
-SHASUM=`shasum -a 256 $BINARY_FILE_NAME | tee aerospace-marks.log | awk '{print $1}'`
+AMD_SHASUM=`shasum -a 256 $AMD_BINARY_FILE_NAME | tee aerospace-marks.log | awk '{print $1}'`
+ARM_SHASUM=`shasum -a 256 $ARM_BINARY_FILE_NAME | tee aerospace-marks.log | awk '{print $1}'`
 
 echo "class AerospaceMarks < Formula
 
   desc 'AeroSpace marks - Vim like marks for AeroSpace WM'
   homepage 'https://github.com/cristianoliveira/aerospace-marks'
-  url '$REPO_URL'
   version '$VERSION'
-  sha256 '$SHASUM'
 
+  if Hardware::CPU.arm?
+    url '${ARM_REPO_URL}'
+    sha256 '${ARM_SHASUM}'
+  else
+    url '${AMD_REPO_URL}'
+    sha256 '${AMD_SHASUM}'
+  end
 
   def install
     bin.install 'aerospace-marks'
+  end
+
+  test do
+    system "#{bin}/aerospace-marks", "--version"
   end
 
 end" > aerospace-marks.rb
