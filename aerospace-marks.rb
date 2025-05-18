4aadb70086318371a5df0d4668b5a9c2ddffacd9
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-18 21:21:00 +0200

    chore: aerospace-marks version v0.0.1

---
+++ aerospace-marks.rb
@@ -0,0 +1,14 @@
+class AeroSpaceMarks < Formula
+
+  desc 'This is an cli for AeroSpace WM to manage marks on windows.'
+  homepage 'https://github.com/cristianoliveira/aerospace-marks'
+  url 'https://github.com/cristianoliveira/aerospace-marks/releases/download/v0.0.1/aerospace-marks-v0.0.1-darwin-amd64.tar.gz'
+  version 'v0.0.1'
+  sha256 'c584f9ad7aaef76e31b0ee9ff38cdd4739217341d2dac3cf585117c2e7afca08'
+
+
+  def install
+    bin.install 'aerospace-marks'
+  end
+
+end
