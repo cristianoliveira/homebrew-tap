74edae51c6a8d87de5318f1146350e5473485e36
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-18 22:32:07 +0200

    chore: aerospace-marks version v0.0.1

--- aerospace-marks.rb
+++ aerospace-marks.rb
@@ -5,11 +5,11 @@ class AerospaceMarks < Formula
   version 'v0.0.1'
 
   if Hardware::CPU.arm?
-    url https://github.com/cristianoliveira/aerospace-marks/releases/download/v0.0.1/
-    sha256 
+    url 'https://github.com/cristianoliveira/aerospace-marks/releases/download/v0.0.1/aerospace-marks-v0.0.1-darwin-arm64.tar.gz'
+    sha256 'b919322a925dac6bfdc9f2990a29ef9fc60ed7080d8ede2efb23242bfe4d2237'
   else
-    url https://github.com/cristianoliveira/aerospace-marks/releases/download/v0.0.1/
-    sha256 c584f9ad7aaef76e31b0ee9ff38cdd4739217341d2dac3cf585117c2e7afca08
+    url 'https://github.com/cristianoliveira/aerospace-marks/releases/download/v0.0.1/aerospace-marks-v0.0.1-darwin-amd64.tar.gz'
+    sha256 'c584f9ad7aaef76e31b0ee9ff38cdd4739217341d2dac3cf585117c2e7afca08'
   end
 
   def install
