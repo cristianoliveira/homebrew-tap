75d51bff0711fd57fc09d52a7ec4b8bd7776225c
cristianoliveira <contato@cristianoliveira.com.br>
2025-06-03 21:32:06 +0200

    chore: aerospace-marks version v0.2.1

--- aerospace-marks.rb
+++ aerospace-marks.rb
@@ -2,14 +2,14 @@ class AerospaceMarks < Formula
 
   desc 'AeroSpace marks - Vim like marks for AeroSpace WM'
   homepage 'https://github.com/cristianoliveira/aerospace-marks'
-  version 'v0.2.0'
+  version 'v0.2.1'
 
   if Hardware::CPU.arm?
-    url 'https://github.com/cristianoliveira/aerospace-marks/releases/download/v0.2.0/aerospace-marks-v0.2.0-darwin-arm64.tar.gz'
-    sha256 '407e1140bea94e76b77c39cd71f56299878f2e1c517da72639be7071c852bb62'
+    url 'https://github.com/cristianoliveira/aerospace-marks/releases/download/v0.2.1/aerospace-marks-v0.2.1-darwin-arm64.tar.gz'
+    sha256 '5fba563a9b1c4625589c95ba962d34c7b4ab26972a4f4e3cc27acdcd5c2b493c'
   else
-    url 'https://github.com/cristianoliveira/aerospace-marks/releases/download/v0.2.0/aerospace-marks-v0.2.0-darwin-amd64.tar.gz'
-    sha256 'b8864a54545716d60fd2e3c044b75063df7ae70ade71a03e4b006ce19c207ea3'
+    url 'https://github.com/cristianoliveira/aerospace-marks/releases/download/v0.2.1/aerospace-marks-v0.2.1-darwin-amd64.tar.gz'
+    sha256 'c64cc29127a375a05dbd46732ec1810c1440164d9c9883306578a0a7ace176a3'
   end
 
   def install
