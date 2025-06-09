dcbf7969ec26b8c1537faefd6694dc3f9feee1ec
cristianoliveira <contato@cristianoliveira.com.br>
2025-06-09 18:02:01 +0200

    chore: aerospace-scratchpad version v0.1.3

--- aerospace-scratchpad.rb
+++ aerospace-scratchpad.rb
@@ -2,14 +2,14 @@ class AerospaceScratchpad < Formula
 
   desc 'AeroSpace scratchpad - Scratchpad for AeroSpace WM'
   homepage 'https://github.com/cristianoliveira/aerospace-scratchpad'
-  version 'v0.1.2'
+  version 'v0.1.3'
 
   if Hardware::CPU.arm?
-    url 'https://github.com/cristianoliveira/aerospace-scratchpad/releases/download/v0.1.2/aerospace-scratchpad-v0.1.2-darwin-arm64.tar.gz'
-    sha256 '29cd47cdd7c6bb576d256c739dd3f4497b805210d1b2e59c3404faddd515683e'
+    url 'https://github.com/cristianoliveira/aerospace-scratchpad/releases/download/v0.1.3/aerospace-scratchpad-v0.1.3-darwin-arm64.tar.gz'
+    sha256 '17ac6b200d1bbbdd261d0c57ee0006f0d639774f90c53bc2f9ee969e67503868'
   else
-    url 'https://github.com/cristianoliveira/aerospace-scratchpad/releases/download/v0.1.2/aerospace-scratchpad-v0.1.2-darwin-amd64.tar.gz'
-    sha256 '944f18320a34acd6d9887f82bc461a652f7c6c2dfe436bc094ba785044f32885'
+    url 'https://github.com/cristianoliveira/aerospace-scratchpad/releases/download/v0.1.3/aerospace-scratchpad-v0.1.3-darwin-amd64.tar.gz'
+    sha256 '77316b52325da2487de5b19f0a8b1116b1c899a484dcd395919a55f73806de69'
   end
 
   def install
